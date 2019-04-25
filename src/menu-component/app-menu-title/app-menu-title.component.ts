af199c45a06f0aeecac9dedaa5dd6bbcc2b20f75
Manuel Cid <manuel.cid@imatia.com>
2019-04-25 18:05:33 +0200

    Update menu

--- src/menu-component/app-menu-title/app-menu-title.component.ts
+++ src/menu-component/app-menu-title/app-menu-title.component.ts
@@ -1,4 +1,6 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, HostBinding } from '@angular/core';
+
 import { AppMenuTitle } from './app-menu-title.class';
 
 @Component({
@@ -10,11 +12,21 @@ import { AppMenuTitle } from './app-menu-title.class';
     'closed: title-closed'
   ],
   templateUrl: './app-menu-title.component.html',
-  styleUrls: ['./app-menu-title.component.scss']
+  styleUrls: ['./app-menu-title.component.scss'],
+  animations: [
+    trigger('toggle', [
+      state('opened', style({})),
+      state('closed', style({
+        transform: 'rotate(180deg)'
+      })),
+      transition('opened <=> closed', animate('0.15s'))
+    ])
+  ]
 })
 export class AppMenuTitleComponent extends AppMenuTitle {
+
   @HostBinding('class.nolabels')
-  onlyIcons: boolean;
+  public onlyIcons: boolean;
 
   constructor() {
     super();
